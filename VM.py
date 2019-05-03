1a2dc08ea237a274b56193405c6df926331b7047
Ryan Broman <ryanjbroman@gmail.com>
2019-05-02 22:33:00 -0700

    Op 0, 1, 6, 7, 8, 9, 19, 21

--- VM.py
+++ VM.py
@@ -10,7 +10,7 @@
 #   add r0, 4, r1 
 #   mov sysout, r0
 #   syscall
-
+import numpy    # numpy makes loading the binary file extremely easier
 class VM:
     def __init__(self):
         # The only way to start is to get started
@@ -21,5 +21,69 @@ class VM:
                                     # creating a whole new array would be another linear construction
         self.counter = 0            # Program counter. Increment the counter for every operation by the amount of operands
                                     # in an instruction + 1 for the opcode
+        self.is_running = False
 
-        
+    def run(self):
+        self.is_running = True
+        f = numpy.fromfile("challenge.bin".encode(), dtype=numpy.dtype("<u2"))
+        # Loads the binary file as an array of readable ints
+        self.memory[0:len(f)] = f.copy() # Loads the copy of f into memory
+        while self.is_running:
+            self.opcodes[self.memory[self.counter]](self)
+    def get_r(self, i):
+        return self.memory[self.counter + i] 
+    def get_operand(self, i):
+        op = self.memory[self.counter + i]
+        if op >= 32768:
+            return self.memory[op]
+        return op
+    
+    # Opcodes 
+
+    def halt(self): # op 0
+        print("Halting!")
+        self.is_running = False
+    
+    def set_r(self): # op 1
+        self.memory[self.get_r(1)] = self.get_operand(2)
+        self.counter += 3
+
+    def jmp(self): # op 6
+        self.counter = self.get_operand(1)
+
+    def jt(self): # op 7
+        if self.get_operand(1) != 0:
+            self.counter = self.get_operand(2)
+        else:
+            self.counter += 3
+    def jf(self): # op 8
+        if self.get_operand(1) == 0:
+            self.counter = self.get_operand(2)
+        else:
+            self.counter += 3
+    def add(self): # op 9
+        tot = self.get_operand(2) + self.get_operand(3)
+        self.memory[self.get_r(1)] = tot % 32768
+        self.counter += 4
+    def out(self): # op 19
+        char = self.get_operand(1)
+        print(chr(char), end="")
+        self.counter += 2
+
+    def noop(self): # op 21
+        self.counter += 1
+
+    opcodes = {
+        0: halt,
+        1: set_r,
+        6: jmp,
+        7: jt,
+        8: jf,
+        9: add,
+        19: out,
+        21: noop,
+    }
+
+
+x = VM()
+x.run()
